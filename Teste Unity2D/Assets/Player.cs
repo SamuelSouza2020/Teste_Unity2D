5caf1674dc040f7944517975feab5fd248137ed1
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-01-20 11:04:58 -0300

    Layout

--- Teste Unity2D/Assets/Player.cs
+++ Teste Unity2D/Assets/Player.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rig;
     void Start()
     {
+        rig = GetComponent<Rigidbody2D>();
         transform.position += (Random.value >= 0.5f) ? (new Vector3(1f, 0)) : (new Vector3(-1f, 0));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+            Empurrao(10, 5);
     }
+    void Empurrao(float x, float y)
+    {
+        rig.AddForce(new Vector2(x,y), ForceMode2D.Impulse);
+    }
+
 }
