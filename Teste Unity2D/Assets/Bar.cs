e1f5b02f0e542bda79dd8b571737c259977e81ee
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-01-20 10:46:53 -0300

    Controle player

--- Teste Unity2D/Assets/Bar.cs
+++ Teste Unity2D/Assets/Bar.cs
@@ -6,22 +6,39 @@ public class Bar : MonoBehaviour
 {
     [SerializeField]
     GameObject lEsq, lDir;
+    Rigidbody2D rigEsq, rigDir;
+    float tqForce = 1000;
+
     void Start()
     {
-        
+        rigEsq = lEsq.GetComponent<Rigidbody2D>();
+        rigDir = lDir.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            AddTorque(rigEsq, tqForce);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            AddTorque(rigDir, -tqForce);
+        }
+        /*if (Input.GetKey(KeyCode.LeftArrow))
         {
             lEsq.transform.eulerAngles += (lEsq.transform.eulerAngles.z <= 50 || lEsq.transform.eulerAngles.z > 340) ? (new Vector3(0, 0, 10)) : (new Vector3(0, 0, 0));
         }
         else
         {
             lEsq.transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, 0, 0), 0.05f * Time.deltaTime);
-        }
-        
+        }*/
+
+    }
+    void AddTorque(Rigidbody2D rigid, float force)
+    {
+        rigid.AddTorque(force);
+        Debug.Log(force);
     }
 }
